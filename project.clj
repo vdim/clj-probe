f04574d385a5e4633caf795e98c29ac9b4afdb64
Vyacheslav Dimitrov <dimitrov@cs.karelia.ru>
2011-07-26 12:56:10 +0400

    Added infrastructure for using leiningen

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clj-probe "0.0.1"
+  :description "Code for testing different features and libraries of Clojure."
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [fnparse "2.2.7"]]
+  :dev-dependencies [[lein-clojars "0.5.0"]])
+
