b1d866018192c1fef1ca248bcc86b6c2e872beb3
Vyacheslav Dimitrov <dimitrov@cs.karelia.ru>
2011-07-26 10:33:02 +0400

    Addes support numbers with more than one digits

--- src/name/vdim/expr.clj
+++ src/name/vdim/expr.clj
@@ -36,10 +36,10 @@
 
 (declare expr)
 (def f (alt (conc (lit \() expr (lit \)))
-            (complex [dig digit,
+            (complex [number (rep+ digit),
                       res (get-info :result)
-                      si (set-info :result (cons (Integer/parseInt (.toString dig)) res))]
-                     dig)))
+                      si (set-info :result (cons (Integer/parseInt (apply str number)) res))]
+                     number)))
 
 (def t-prime (alt (conc 
                      (lit \*) 
@@ -71,6 +71,7 @@
 
 
 (deftest t-doexpr
+         ^{:doc "Checks simple expression"}
          (is (= (doexpr "2") 2))
          (is (= (doexpr "1+0") 1))
          (is (= (doexpr "1+1") 2))
@@ -82,6 +83,16 @@
          (is (= (doexpr "(1+3)*2") 8))
          (is (= (doexpr "(1*5)*4") 20))
          (is (= (doexpr "(0+1)*(3+2)*(0+1)*(0+1)") 5)))
+
+
+(deftest t-doexpr-number
+         ^{:doc "Checks expression with number containing more than one digis."}
+         (is (= (doexpr "10+20") 30))
+         (is (= (doexpr "10*20") 200))
+         (is (= (doexpr "90+110") 200))
+         (is (= (doexpr "90+1") 91))
+         (is (= (doexpr "100000+20") 100020))
+         (is (= (doexpr "(10+1)*(90+10)") 1100)))
 
 (time (run-tests))
 
