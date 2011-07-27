21851633e2c3b43cf08aaf4d585b72c4260a6cc3
vdim <vyacheslav.dimitrov@gmail.com>
2011-07-27 14:55:58 +0400

    Factored out some repeated code

--- src/name/vdim/expr.clj
+++ src/name/vdim/expr.clj
@@ -8,9 +8,9 @@
 ;;
 ;;  BNF without left recursion:
 ;;    E -> T E'
-;;    E'-> + T E'|-T E'| ε
+;;    E'-> + T E' | - T E' | ε
 ;;    T -> F T'
-;;    T'-> * F T'| ε
+;;    T'-> * F T' | / F E' | ε
 ;;    F -> (E) | digit
 ;;    digit -> [0-9]+
 
@@ -23,6 +23,7 @@
 ; is stored in :result.
 (defstruct my-state-s :remainder :result :fn)
 
+; Some helper functions.
 (defmacro create-fn
   [oper & args]
   `(~(symbol (.toString oper)) ~@args))
@@ -34,6 +35,21 @@
   [f res]
   (cons (f (second res) (first res)) (rest (rest res))))
 
+(defn- parse-op
+  "Creates parser for some operation (ch as character of operation
+  and op as function of operation)."
+  [ch p p-prime op]
+  (conc 
+    (lit ch)
+    (complex 
+      [ff p
+       res (get-info :result)
+       si (set-info :result (do-eval op res))]
+      ff) 
+    p-prime))
+
+
+; Rules of grammar are below. See BNF in the begining of file.
 (def digit (lit-alt-seq "0123456789"))
 
 (def double-number
@@ -47,51 +63,23 @@
                       si (set-info :result (cons (Double/parseDouble (apply str (flatten number))) res))]
                      number)))
 
-(def t-prime (alt (conc 
-                    (lit \*)
-                     (complex 
-                       [ff f
-                        res (get-info :result)
-                        si (set-info :result (do-eval * res))]
-                       ff) 
-                     t-prime) 
-                  (conc 
-                    (lit \/)
-                     (complex 
-                       [ff f
-                        res (get-info :result)
-                        si (set-info :result (do-eval / res))]
-                       ff) 
-                     t-prime) 
-
-                  emptiness))
+(def t-prime (alt 
+               (parse-op \* f t-prime *)
+               (parse-op \/ f t-prime /)
+               emptiness))
 
 (def t (conc f t-prime))
 
-(def e-prime (alt (conc
-                    (lit \+)
-                     (complex 
-                       [tt t
-                        res (get-info :result)
-                        si (set-info :result (do-eval + res))]
-                       tt)                    
-                     e-prime) 
-                  (conc
-                    (lit \-)
-                     (complex 
-                       [tt t
-                        res (get-info :result)
-                        si (set-info :result (do-eval - res))]
-                       tt)                    
-                     e-prime) 
-                 
-                  emptiness))
+(def e-prime (alt 
+               (parse-op \+ t e-prime +)
+               (parse-op \- t e-prime -)
+               emptiness))
 
 (def expr (conc t e-prime))
 
+; Function for using grammar.
 (defn doexpr
   "Computes simple arithmetic expression."
   [e]
   (first (:result ((expr (struct my-state-s (seq e) ())) 1))))
 
-(doexpr "1-3*2")
