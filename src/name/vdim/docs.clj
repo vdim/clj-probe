9db3b380bf59439de5248252e8fb4b5c40a2ca4c
vdim <vyacheslav.dimitrov@gmail.com>
2011-08-08 23:38:28 +0400

    Added filter-ns function

--- src/name/vdim/docs.clj
+++ src/name/vdim/docs.clj
@@ -8,12 +8,18 @@
   ^{:doc "Docs library is improvement of the find-doc function from clojure.core"}
   [:use clojure.contrib.ns-utils])
 
+(defn filter-ns
+  "Filters list of all namespaces by specified regex."
+  [re]
+  (if re
+    (filter #(re-seq (re-pattern re) (-> % .getName name)) (all-ns))
+    (all-ns)))
+
 (defn find-name
-  "Returns list of Clojure's statements (function, macros and so on) by specified its name
-  (or part of name or some regex)."
-  [n]
+  "Returns list of Clojure's statements (function, macros and so on) 
+  by specified its name (or part of name or some regex)."
+  [n & {:keys [ns]}]
   (flatten
     (map
       #(filter (fn [x] (re-seq (re-pattern n) (str x))) (ns-vars %))
-      (all-ns))))
-
+      (filter-ns ns))))
