3e905a3556f4d792519c3e0b6a29855fee3e6c6c
vdim <vyacheslav.dimitrov@gmail.com>
2011-08-09 13:07:02 +0400

    Improved ifind-doc

--- src/name/vdim/docs.clj
+++ src/name/vdim/docs.clj
@@ -6,20 +6,45 @@
 
 (ns name.vdim.docs
   ^{:doc "Docs library is improvement of the find-doc function from clojure.core"}
-  [:use clojure.contrib.ns-utils])
+  [:use clojure.contrib.ns-utils]
+  [:import java.util.regex.Pattern])
 
-(defn filter-ns
+(defn- f-ns
   "Filters list of all namespaces by specified regex."
   [re]
+  (filter #(re-seq (re-pattern re) (-> % .getName name)) (all-ns)))
+
+(defn filter-ns
+  "Checks input regex (or string, or vector) and then 
+  filters list of all namespaces by f-ns function."
+  [re]
   (if re
-    (filter #(re-seq (re-pattern re) (-> % .getName name)) (all-ns))
+    (if
+      (or (string? re) (= (class re) Pattern)) (f-ns re)
+      (flatten (map #(f-ns %) re)))
     (all-ns)))
 
 (defn find-name
-  "Returns list of Clojure's statements (function, macros and so on) 
-  by specified its name (or part of name or some regex)."
-  [n & {:keys [ns]}]
+  "Returns list of Clojure's statements (function, macros and so on) which are filtered
+  by specified its name (or part of name or some regex).
+  First param is specified regex for name."
+  [n ns]
   (flatten
     (map
-      #(filter (fn [x] (re-seq (re-pattern n) (str x))) (ns-vars %))
+      #(map (fn [x] (ns-resolve % x))  
+            (filter (fn [x] (re-seq (re-pattern n) (str x))) (ns-vars %)))
       (filter-ns ns))))
+
+(defn ifind-doc
+  "Extented version of find-doc function.
+  First param is specified regex for name.
+  Then different settings is followed.
+    :ns <pattern> - define namespace(s) where name is searched.
+    :settings (must be set for correct getting) - different settings, which are include:
+        :print-doc - define whether doc-string for symbol is printed."
+  [re-or-s-or-map & {:keys [ns settings]}]
+  (let [lvars (find-name re-or-s-or-map ns)]
+    (if (contains? settings :print-doc)
+      (doseq [item lvars] (print-doc item))
+      lvars)))
+
