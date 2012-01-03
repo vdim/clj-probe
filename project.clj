ba525c64b2182794373b286fb26fff12e97983b1
vdim <vyacheslav.dimitrov@gmail.com>
2012-01-03 20:46:50 +0300

    Added init script for "lein repl" command.

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [fnparse "2.2.7"]]
-  :dev-dependencies [[lein-clojars "0.5.0"]])
+  :dev-dependencies [[lein-clojars "0.5.0"]] 
+  :repl-init name.vdim.init)
 
---
+++ src/name/vdim/init.clj
@@ -0,0 +1,8 @@
+;; Counting some statistic from file with developer's working hours, e.g.
+;; sum of hours and etc.
+
+(ns name.vdim.init
+  (:use name.vdim.whcount))
+
+(get-stats "/home/adim/tsen/notes/wh-Dimitrov.txt")
+
